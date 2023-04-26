2184da203f38188e26ed831ced6dc84c3c5bc27a
Filipa Geraldes <116494634+g3raldes@users.noreply.github.com>
2023-04-26 15:08:16 +0100

    Update Image.cpp

--- Image.cpp
+++ Image.cpp
@@ -2,19 +2,20 @@
 
 namespace prog
 {
-  Image::Image(int w, int h, const Color &fill)
+  Image::Image(int w, int h, const Color &fill) : : width_(w), height_(h), pixels_(w * h, fill)
   {
+  
   }
   Image::~Image()
   {
   }
   int Image::width() const
   {
-    return -1;
+    return width_;
   }
   int Image::height() const
   {
-    return -1;
+    return height_;
   }
 
   // TODO: remove this DUMMY_color variable once you have appropriate fields for representing image pixels.
@@ -22,11 +23,11 @@ namespace prog
 
   Color& Image::at(int x, int y)
   {
-    return DUMMY_color;
+    return pixels_[y*width_ + x];
   }
 
   const Color& Image::at(int x, int y) const
   {
-    return DUMMY_color;
+    return pixels_[y*width_ + x];
   }
 }
