1e39adc6dd550955a916997a35b5f4226a9feb31
Filipa Geraldes <116494634+g3raldes@users.noreply.github.com>
2023-04-26 14:43:52 +0100

    Update Image.hpp

--- Image.hpp
+++ Image.hpp
@@ -7,14 +7,43 @@ namespace prog
   class Image
   {
   private:
-    // TODO: define private fields for image state
+    int width_;
+    int height_;
+    Color **pixels_;
   public:
-    Image(int w, int h, const Color &fill = {255, 255, 255});
-    ~Image();
-    int width() const;
-    int height() const;
-    Color &at(int x, int y);
-    const Color &at(int x, int y) const;
+    Image(int w, int h, const Color &fill = {255, 255, 255}) :
+      width_(w),
+      height_(h),
+      pixels_(new Color *[h])
+    {
+      for (int i = 0; i < h; i++) {
+        pixels_[i] = new Color[w];
+        for (int j = 0; j < w; j++) {
+          pixels_[i][j] = fill;
+        }
+      }
+    ~Image() 
+    {
+      for (int i = 0; i < height_; i++) {
+        delete[] pixels_[i];
+      }
+      delete[] pixels_;
+    }
+    int width() const
+    {
+      return width_;
+    }
+    int height() const {
+      return height_;
+    }
+    Color &at(int x, int y)
+    {
+      return pixels_[y][x];
+    }
+    const Color &at(int x, int y) const 
+    {
+      return pixels_[y][x];
+    }
   };
 }
 #endif
