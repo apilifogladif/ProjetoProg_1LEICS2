d78e2f18e29aa3fa422c0dd639a020a696549ad3
Filipa Geraldes <116494634+g3raldes@users.noreply.github.com>
2023-04-26 14:54:16 +0100

    Update Image.hpp

--- Image.hpp
+++ Image.hpp
@@ -1,49 +1,24 @@
 #ifndef __prog_Image_hpp__
 #define __prog_Image_hpp__
 #include "Color.hpp"
+#include <vector>
 
 namespace prog
 {
   class Image
   {
   private:
+    // TODO: define private fields for image state
     int width_;
     int height_;
-    Color **pixels_;
+    std::vector<std::vector<Color>> pixels_;
   public:
-    Image(int w, int h, const Color &fill = {255, 255, 255}) :
-      width_(w),
-      height_(h),
-      pixels_(new Color *[h])
-    {
-      for (int i = 0; i < h; i++) {
-        pixels_[i] = new Color[w];
-        for (int j = 0; j < w; j++) {
-          pixels_[i][j] = fill;
-        }
-      }
-    ~Image() 
-    {
-      for (int i = 0; i < height_; i++) {
-        delete[] pixels_[i];
-      }
-      delete[] pixels_;
-    }
-    int width() const
-    {
-      return width_;
-    }
-    int height() const {
-      return height_;
-    }
-    Color &at(int x, int y)
-    {
-      return pixels_[y][x];
-    }
-    const Color &at(int x, int y) const 
-    {
-      return pixels_[y][x];
-    }
+    Image(int w, int h, const Color &fill = {255, 255, 255});
+    ~Image();
+    int width() const;
+    int height() const;
+    Color &at(int x, int y);
+    const Color &at(int x, int y) const;
   };
 }
 #endif
